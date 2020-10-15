e9ea40705f73d305815785c83323f03b8aecdea5
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-15 17:11:43 +0800

    [MarsRover][Vea] feature - 0,0 N

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,38 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    private int posX;
+    private int posY;
+    private String direction;
+
+    public MarsRover(int posX , int posY, String direction) {
+        this.posX = posX;
+        this.posY = posY;
+        this.direction = direction;
+    }
+
+    public void moveMarsRover(String movement) {
+        if(movement.equals("M")){
+            moveFoward();
+        }
+    }
+
+    private void moveFoward() {
+        if(direction.equals("N")){
+            posY += 1;
+        }
+    }
+
+    public int getXPosition() {
+        return posX;
+    }
+
+    public int getYPosition(){
+        return posY;
+    }
+
+    public String getDirection(){
+        return direction;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,19 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
-class DemoTest {
-    @Test
-    void write_your_first_test() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class DemoTest {
+
+    @Test
+    void test_when_x0_y0_heading_N_with_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        //when
+        marsRover.moveMarsRover("M");
+        //then
+        assertEquals(0, marsRover.getXPosition());
+        assertEquals(1, marsRover.getYPosition());
+        assertEquals("N", marsRover.getDirection());
     }
 }
