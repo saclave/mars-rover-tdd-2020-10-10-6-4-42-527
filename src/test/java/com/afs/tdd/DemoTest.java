3fae6c60a70d29636bacfea4bdaa6edc45640fd6
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-15 18:00:27 +0800

    [MarsRover][Vea] test - 0,0 W L

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -126,4 +126,16 @@ class DemoTest {
         assertEquals(0, marsRover.getYPosition());
         assertEquals("W", marsRover.getDirection());
     }
+
+    @Test
+    void test_when_x0_y0_heading_W_with_command_L(){
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        //when
+        marsRover.moveMarsRover("L");
+        //then
+        assertEquals(0, marsRover.getXPosition());
+        assertEquals(0, marsRover.getYPosition());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
